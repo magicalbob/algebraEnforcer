caf57ec65cbb9c3545ae6c399b0683a34666992c
ian <ellisia@me.com>
2015-10-11 12:29:00 +0100

    Added some sys logging

--- algebraui.py
+++ algebraui.py
@@ -12,6 +12,7 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash, _app_ctx_stack
 from algebra import Algebra
 import os
+import syslog
 
 # configuration
 DEBUG = True
@@ -28,6 +29,7 @@ app.config.from_object(__name__)
 @app.route('/',methods=['GET','POST'])
 def start_up():
   if request.method == 'GET':
+    syslog.syslog("Algebra: Request for '/' from %s" % (request.environ['REMOTE_ADDR']))
     session['_q_count']=1
     return render_template('start_up.html')
   else:
@@ -46,6 +48,7 @@ def makeQ(new_quest):
       xAlg.disp2=session['_alg_disp_2']
       xAlg.x=session['_alg_x']
       xAlg.y=session['_alg_y']
+    syslog.syslog("Algebra: New question { Q1: %s, Q2: %s, X = %s, Y = %s } from %s" % (session['_alg_disp_1'], session['_alg_disp_2'], session['_alg_x'], session['_alg_y'], request.environ['REMOTE_ADDR']))
     print "Q1: %s" % (session['_alg_disp_1'])
     print "Q2: %s" % (session['_alg_disp_2'])
     print "X: %s" % (session['_alg_x'])
@@ -67,6 +70,7 @@ def show_answer():
     session['_q_count']+=1 
     if session['_q_count'] > 3:
       os.system("salt 'tc600' cmd.run 'route add 0.0.0.0 mask 0.0.0.0 192.168.2.1'")
+      syslog.syslog("Algebra: Internet restored by %s" % (request.environ['REMOTE_ADDR']))
       return render_template('well_done.html')
     else:
       flash("Well done that's right. Here's another one.")
